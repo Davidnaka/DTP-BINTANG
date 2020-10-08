4f48d310265d622c6f7220fc9ccfb6f271b6cf6b
Davidnaka <72536148+Davidnaka@users.noreply.github.com>
2020-10-08 11:35:07 +0700

    Add files via upload

---
+++ Tugasdavid3.js
@@ -0,0 +1,24 @@
+var baris = 3;
+var polaBintang = "*";
+var polaSamaDgn = "=";
+var hasil = "";
+
+for (let indexx = 1; indexx < 6; indexx++) {
+	if ((indexx %2)==0) {
+	hasil = hasil.concat(polaSamaDgn);
+	} else { 
+	hasil = hasil.concat(polaBintang);
+	}
+}
+console.log(""+hasil);
+
+hasil="";
+for (let indexx = 1; indexx < 4; indexx++) {
+	if ((indexx %2)==0) {
+	hasil = hasil.concat(polaSamaDgn);
+	} else { 
+	hasil = hasil.concat(polaBintang);
+	}
+}
+console.log(" "+hasil);
+console.log("  "+polaBintang);
